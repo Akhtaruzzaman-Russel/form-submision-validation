b2693b6ede326caf93c6762c3dc35ccc9856e56a
KaziRussel <kaziakhtaruzzaman22@gmail.com>
2025-11-02 12:09:07 -0800

    021120250208

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,21 +6,38 @@ const Form = () => {
     const [email , setEmail] = useState("")
     const [message , setMessage] = useState("")
 
+    const [errors , setErrors] = useState("")
+
     // console.log("Name:", name)
     // console.log("Email:", email)
     // console.log("Message:", message)
+    // const newErrors = { name: "Name is Required", email: "Invalid Email, Please correct it." message: "Message cannot be empty!" }
 
     // Submit Form handler
     const handleSubmit = (e)=>{
-        e.preventDefault();
-        alert(`Hi, ${name}! Thanks for Submitting the Form`)
-        setName("")
-        setEmail("")
-        setMessage("")
 
+             e.preventDefault();
 
+        if (validateForm()){
+                alert(`Hi, ${name}! Thanks for Submitting the Form`)
+                setName("")
+                setEmail("")
+                setMessage("")
+
+        }
     }
 
+    // check form validation 
+    const validateForm = () => {
+
+        const newErrors = {};
+        if(!name.trim()) newErrors.name = "Name is Required"
+        if(!email.trim() || !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) newErrors.email = "Invalid Email, Please correct it."
+        if(!message.trim()) newErrors.message = "Message cannot be empty!"
+        setErrors(newErrors)
+        return Object.keys(newErrors).length === 0  
+        
+    }
 
   return (
     <form onSubmit={handleSubmit} className='max-w-md mx-auto bg-white text-black p-8 rounded space-y-3'>
@@ -36,6 +53,10 @@ const Form = () => {
             id="name" 
             placeholder='Enter Your Name' 
             className='border p-2 md:w-64 w-full'/>
+
+            {
+                errors.name && <p className='text-red-500 text-sm italic mt-1'>{errors.name }</p>
+            }
         </div>
         {/* Email Field */}
 
@@ -49,6 +70,10 @@ const Form = () => {
             id="email" 
             placeholder='Enter Your Email' 
             className='border p-2 md:w-64 w-full'/>
+
+            {
+                errors.email && <p className='text-red-500 text-sm italic mt-1'>{ errors.email }</p>
+            }
         </div>
 
         {/* text Message Area */}
@@ -63,6 +88,9 @@ const Form = () => {
             id="massage" 
             placeholder='Type your message here...' 
             className='border p-2 md:w-64 w-full' ></textarea>
+            {
+                errors.message && <p className='text-red-500 text-sm italic mt-1'>{ errors.message }</p>
+            }
         </div>
         <button type="submit" className='bg-blue-600 px-6 py-2 text-white rounded'>Submit</button>
         
